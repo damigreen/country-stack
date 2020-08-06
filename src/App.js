60794ac010388ea3b1a145093692235b281a0e30
Damilola Faseun <fashfired@gmail.com>
2020-08-06 16:47:58 +0100

    Get countries from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Find from './components/Find';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([]);
+    const [countryName, setCountryName] = useState('');
+
+    // make a HTTP request to the restcountries API and use the
+    // javascrit promse to set the API responce to the variale name countries
+    const hook = (() => {
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then((response) => {
+                console.log(response.data);
+                setCountries(response.data);
+            });
+    });
+    useEffect(hook, []);
+
+    // const defaultCountry = countries.find(country => country.name === 'Nigeria');
+
+
+    return (
+        <div>
+            <h2>Countries</h2>
+            <Find
+                countries={countries}
+                countryName={countryName}
+                setCountryName={setCountryName} />
+        </div>
+    );
 }
 
 export default App;
