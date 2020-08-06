14b651df6b69ab0bc0ff16f7a9012b20cbaea517
Damilola Faseun <fashfired@gmail.com>
2020-08-06 17:03:03 +0100

    Updates

---
+++ precache-manifest.e516649e82c7733bde1c8ed9ed371ab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a39c2b40109d9a5071a4b1548c04339",
+    "url": "/country-stack/index.html"
+  },
+  {
+    "revision": "88210dffc1c41c14f7b9",
+    "url": "/country-stack/static/js/2.b0695cb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-stack/static/js/2.b0695cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf5a2d1aca58fde84fc",
+    "url": "/country-stack/static/js/main.7efe4068.chunk.js"
+  },
+  {
+    "revision": "0ef902af09017ea0504e",
+    "url": "/country-stack/static/js/runtime-main.f5a58acf.js"
+  }
+]);
